bbaae161e3f13166437aa0910d79c4a7bceffda8
theonetop <ilue00@gmail.com>
2023-05-10 21:07:59 +0900

    abc 브랜치 이동하기전 commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 
-# 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
+# # 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
+# https://theonetop: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@github.com/theonetop/test-repo.git/
 
 def hello(name):
     print('hello' + name)
-    print('테스트')
+    # print('테스트')
 
